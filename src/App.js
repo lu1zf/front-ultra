ccb129d4e19a1e3f92ff9d6f72f299cd680ab429
Luiz Silva <luizsilva.ee@gmail.com>
2021-12-05 22:28:24 -0300

    Instancia estilos globais

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Navbar } from './components/index';
+import GlobalStyle from './styles/globalStyles';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <GlobalStyle />
     </Router>
   );
 }
