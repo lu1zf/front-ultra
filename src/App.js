1c8a0422f663757ac139179b2969b48dd8d83fa2
Luiz Silva <luizsilva.ee@gmail.com>
2021-12-07 00:01:22 -0300

    Adiciona rota de homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import { Navbar } from './components/index';
+import { Navbar, Footer } from './components/index';
 import GlobalStyle from './styles/globalStyles';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home  from './pages/HomePage/Home';
 
 function App() {
   return (
     <Router>
-      <Navbar />
       <GlobalStyle />
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+      </Routes>
+      <Footer />
     </Router>
   );
 }
