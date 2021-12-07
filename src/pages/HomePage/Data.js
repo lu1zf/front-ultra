af037c2fd66cf80966990a18e759f1ef5abede40
Luiz Silva <luizsilva.ee@gmail.com>
2021-12-07 00:02:01 -0300

    Cria objetos de dados

---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,63 @@
+export const homeObjOne = {
+  primary: true,
+  lightBg: false, 
+  imgStart: '',
+  lightTopLine: true,
+  lightTextDesc: true,
+  buttonLabel: 'Get Started',
+  description: 'We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals',
+  headLine: 'Lead Generation Specialist for Online Businesses',
+  lightText: true, 
+  topLine: 'Marketing Agency',
+  start: '',
+  src: require('../../images/svg-1.svg').default,
+  alt: 'Image',
+}
+
+export const homeObjTwo = {
+  primary: false,
+  lightBg: true, 
+  imgStart: 'start',
+  lightTopLine: false,
+  lightTextDesc: false,
+  buttonLabel: 'Get Started',
+  description: 'We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals',
+  headLine: 'Lead Generation Specialist for Online Businesses',
+  lightText: false, 
+  topLine: 'Marketing Agency',
+  start: 'true',
+  src: require('../../images/profile.jpg').default,
+  alt: 'Image',
+}
+
+export const homeObjThree = {
+  primary: true,
+  lightBg: false, 
+  imgStart: '',
+  lightTopLine: true,
+  lightTextDesc: true,
+  buttonLabel: 'Get Started',
+  description: 'We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals',
+  headLine: 'Lead Generation Specialist for Online Businesses',
+  lightText: true, 
+  topLine: 'Marketing Agency',
+  start: '',
+  src: require('../../images/svg-2.svg').default,
+  alt: 'Image',
+}
+
+export const homeObjFour = {
+  primary: true,
+  lightBg: false, 
+  imgStart: 'start',
+  lightTopLine: true,
+  lightTextDesc: true,
+  buttonLabel: 'Get Started',
+  description: 'We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals',
+  headLine: 'Lead Generation Specialist for Online Businesses',
+  lightText: true, 
+  topLine: 'Marketing Agency',
+  start: 'true',
+  src: require('../../images/svg-3.svg').default,
+  alt: 'Image',
+}
