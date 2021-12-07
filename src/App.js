3d0f2d9c7ef81086c25c08a9a51a692300ff1c3b
Luiz Silva <luizsilva.ee@gmail.com>
2021-12-07 06:40:42 -0300

    Cria rotas

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import { Navbar, Footer } from './components/index';
 import GlobalStyle from './styles/globalStyles';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home  from './pages/HomePage/Home';
+import Services from './pages/Services/Services';
+import Products from './pages/Products/Products';
+import SignUp from './pages/SignUp/SignUp';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 
 function App() {
   return (
@@ -11,6 +15,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route exact path='/' element={<Home />} />
+        <Route exact path='/services' element={<Services />} />
+        <Route exact path='/products' element={<Products />} />
+        <Route exact path='/sign-up' element={<SignUp />} />
       </Routes>
       <Footer />
     </Router>
